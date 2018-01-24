a02a79a67b92091e3d7dc9fbd7a35c8080045750
Andrey Yeschenko <30596007+andryes@users.noreply.github.com>
2018-01-24 18:54:59 +0200

    deleted

--- README.md
+++
@@ -1,7 +0,0 @@
-# aws-test
-
-Test application includes several pages with registration and authorization forms. Uses Bootstrap 3, PHP, MySQL.
-
-DB settings can be found in function "connect" (pages/functions.php), MySQL dump - in file "todo.sql.zip".
-
-To be continued
--- pages/about.php
+++
@@ -1,8 +0,0 @@
-<div class="container">
-    <h3 class="about_H">About</h3>
-</div>
-<div class="container about">
-    <p>Test application for learning Bootstrap 3, PHP, and MySQL.</p>
-    <p>Application includes several pages with registration and authorization forms.</p>
-    <p>To be continued</p>
-</div>
